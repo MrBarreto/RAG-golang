653320deded98a3659489692712ad2cb0dabed97
MrBarreto <saviobarteles@poli.ufrj.br>
2025-08-22 00:58:43 -0300

    Testing database connection

--- src/main.go
+++ src/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
-
+	"database/sql"
 	_ "github.com/lib/pq"
+)
+const (
+	host = "timescaledb"
+	port = 5432
+	user = "postgres"
+	password = "password"
+	dbname = "test_db"
 )
 
 func main() {
 	fmt.Println("Teste")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+    		panic(err)
+  	}
+  	defer db.Close()
+
+  	err = db.Ping()
+  	if err != nil {
+    		panic(err)
+  	}
+
+  	fmt.Println("Successfully connected!")
 }
